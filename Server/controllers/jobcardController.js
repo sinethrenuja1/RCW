af7f9e10d8e2d6bd05ab4f74190f0bf0d28f42a2
sinethrenuja1 <sinethrenuja312@gmail.com>
2024-05-15 23:57:03 +0530

    Add register backend and Add FontAwesome icons and update button styling

--- Server/controllers/jobcardController.js
+++ Server/controllers/jobcardController.js
@@ -1,90 +1,182 @@
+// import db from '../db.js';
+
+// //register cusstomer
+// export const registerCustomer = async (req, res) => {
+//     const {contact_number,first_name,last_name,address} = req.body;
+
+//     try{
+//         const q=`INSERT INTO customer_info (contact_number,first_name,last_name,address) VALUES (?,?,?,?)`
+//         await new Promise((resolve, reject) => {
+//             db.query(q, [contact_number,first_name,last_name,address], (err, result) => {
+//                 if (err) {
+//                     reject(err);  // If there's an error, reject the Promise
+//                     console.log(err);
+//                 } else {
+//                     resolve(result); 
+//                     console.log(result);
+//                      // If everything went well, resolve the Promise
+//                 }
+//             });
+//         });
+
+//         res.status(201).json({ message: 'Customer Details added successfully' });
+
+//     }catch(err){
+//         console.error(err);
+//         res.status(500).json({error: 'An unexpected error occurred'});
+//     }
+// }
+
+
+
+
+// //------------------------------------------------------------------------
+// //register vehicledetails
+
+// export const registerVehicle = async (req, res) => {
+//     const { veh_num,make,model,engine_type,contact_number} = req.body;
+
+//     try{
+//         const q=`INSERT INTO vehicle_details (veh_num,make,model,engine_type,contact_number) VALUES (?,?,?,?,?)`
+//         await new Promise((resolve, reject) => {
+//             db.query(q, [veh_num,make,model,engine_type,contact_number], (err, result) => {
+//                 if (err) {
+//                     reject(err);  // If there's an error, reject the Promise
+//                     console.log(err);
+//                 } else {
+//                     resolve(result); 
+//                     console.log(result);
+//                      // If everything went well, resolve the Promise
+//                 }
+//             });
+//         });
+//         ;
+
+//         res.status(201).json({ message: 'Vehicle Details added successfully' });
+//     }catch(err){
+//         console.error(err);
+//         res.status(500).json({error: 'An unexpected error occurred'});
+//     }
+// }
+
+
+// //------------------------------------------------------------------
+// //Search Customer
+// export const searchCustomer = async (req, res) => {
+//     const {contact_number} = req.params;
+
+//     try{
+//         const q = `SELECT * FROM customer_info WHERE contact_number = ?`;
+//         const customer =  await new Promise((resolve, reject) => {
+//             db.query(q, [contact_number], (err, result) => {
+//                 if (err) {
+//                     reject(err);  // If there's an error, reject the Promise
+//                     console.error(err);
+//                 } else {
+//                     resolve(result); 
+//                     console.log(result);
+//                      // If everything went well, resolve the Promise
+//                 }
+//             });
+//         });
+//         if (customer.length > 0) {
+//             res.json(customer[0]);
+//         } else {
+//             res.status(404).json({ message: 'Customer not found' });
+//         }
+//     }catch{
+//         console.error(err);
+//         res.status(500).json({ error: 'An unexpected error occurred' });
+//     }
+// }
+
 import db from '../db.js';
 
-//register cusstomer
+// Register customer
 export const registerCustomer = async (req, res) => {
-    const {contact_number,first_name,last_name,address} = req.body;
+    const { contact_number, first_name, last_name, address } = req.body;
 
-    try{
-        const q=`INSERT INTO customer_info (contact_number,first_name,last_name,address) VALUES (?,?,?,?)`
-        await new Promise((resolve, reject) => {
-            db.query(q, [contact_number,first_name,last_name,address], (err, result) => {
+    try {
+        const checkQuery = `SELECT * FROM customer_info WHERE contact_number = ?`;
+        const customer = await new Promise((resolve, reject) => {
+            db.query(checkQuery, [contact_number], (err, result) => {
                 if (err) {
                     reject(err);  // If there's an error, reject the Promise
-                    console.log(err);
                 } else {
-                    resolve(result); 
-                    console.log(result);
-                     // If everything went well, resolve the Promise
+                    resolve(result);  // If everything went well, resolve the Promise
                 }
             });
         });
 
-        res.status(201).json({ message: 'Customer Details added successfully' });
+        if (customer.length > 0) {
+            // Customer already exists
+            res.status(200).json({ message: 'Customer already exists' });
+        } else {
+            // Insert new customer
+            const insertQuery = `INSERT INTO customer_info (contact_number, first_name, last_name, address) VALUES (?,?,?,?)`;
+            await new Promise((resolve, reject) => {
+                db.query(insertQuery, [contact_number, first_name, last_name, address], (err, result) => {
+                    if (err) {
+                        reject(err);  // If there's an error, reject the Promise
+                    } else {
+                        resolve(result);  // If everything went well, resolve the Promise
+                    }
+                });
+            });
 
-    }catch(err){
+            res.status(201).json({ message: 'Customer details added successfully' });
+        }
+    } catch (err) {
         console.error(err);
-        res.status(500).json({error: 'An unexpected error occurred'});
+        res.status(500).json({ error: 'An unexpected error occurred' });
     }
-}
+};
 
-
-
-
-//------------------------------------------------------------------------
-//register vehicledetails
-
+// Register vehicle
 export const registerVehicle = async (req, res) => {
-    const { veh_num,make,model,engine_type,contact_number} = req.body;
+    const { veh_num, make, model, engine_type, contact_number } = req.body;
 
-    try{
-        const q=`INSERT INTO vehicle_details (veh_num,make,model,engine_type,contact_number) VALUES (?,?,?,?,?)`
+    try {
+        const query = `INSERT INTO vehicle_details (veh_num, make, model, engine_type, contact_number) VALUES (?,?,?,?,?)`;
         await new Promise((resolve, reject) => {
-            db.query(q, [veh_num,make,model,engine_type,contact_number], (err, result) => {
+            db.query(query, [veh_num, make, model, engine_type, contact_number], (err, result) => {
                 if (err) {
                     reject(err);  // If there's an error, reject the Promise
-                    console.log(err);
                 } else {
-                    resolve(result); 
-                    console.log(result);
-                     // If everything went well, resolve the Promise
+                    resolve(result);  // If everything went well, resolve the Promise
                 }
             });
         });
-        ;
 
-        res.status(201).json({ message: 'Vehicle Details added successfully' });
-    }catch(err){
+        res.status(201).json({ message: 'Vehicle details added successfully' });
+    } catch (err) {
         console.error(err);
-        res.status(500).json({error: 'An unexpected error occurred'});
+        res.status(500).json({ error: 'An unexpected error occurred' });
     }
-}
+};
 
-
-//------------------------------------------------------------------
-//Search Customer
+// Search customer
 export const searchCustomer = async (req, res) => {
-    const {contact_number} = req.params;
+    const { contact_number } = req.params;
 
-    try{
-        const q = `SELECT * FROM customer_info WHERE contact_number = ?`;
-        const customer =  await new Promise((resolve, reject) => {
-            db.query(q, [contact_number], (err, result) => {
+    try {
+        const query = `SELECT * FROM customer_info WHERE contact_number = ?`;
+        const customer = await new Promise((resolve, reject) => {
+            db.query(query, [contact_number], (err, result) => {
                 if (err) {
                     reject(err);  // If there's an error, reject the Promise
-                    console.error(err);
                 } else {
-                    resolve(result); 
-                    console.log(result);
-                     // If everything went well, resolve the Promise
+                    resolve(result);  // If everything went well, resolve the Promise
                 }
             });
         });
+
         if (customer.length > 0) {
             res.json(customer[0]);
         } else {
             res.status(404).json({ message: 'Customer not found' });
         }
-    }catch{
+    } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'An unexpected error occurred' });
     }
